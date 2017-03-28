88842ce8cd408e0a74ae8aa0fd36736dc4b234d6
23 <a23@23deimac.local>
2017-03-28 09:47:37 +0800

    更新注释

--- CMChart/CMChart/CMChart/CMHistogramChartView.h
+++ CMChart/CMChart/CMChart/CMHistogramChartView.h
@@ -15,16 +15,39 @@
 @interface CMHistogramChartView : UIView
 
 #pragma mark - API
-/**设置标题*/
+/**
+ 设置标题
+
+ @param mainTitle 主标题
+ @param secondTitle 副标题
+ @param thirdTitle 副副标题
+ */
 - (void)setMainTitle:(NSString *)mainTitle  secondTitle:(NSString *)secondTitle thirdTitle:(NSString *)thirdTitle;
 
-/**设置标题及其属性*/
+/**
+ 设置标题及其标题属性
+
+ @param mainTitle 主标题
+ @param mainAttribute 主标题的属性字典
+ @param secondTitle 副标题
+ @param secondAttribute 副标题的属性字典
+ @param thirdTitle 副副标题
+ @param thirdAttribute 副副标题的属性字典
+ */
 - (void)setMainTitle:(NSString *)mainTitle mainTitleAttribute:(NSDictionary *)mainAttribute secondTitle:(NSString *)secondTitle secondTitleAttribute:(NSDictionary *)secondAttribute thirdTitle:(NSString *)thirdTitle thirdTitleAttribute:(NSDictionary *)thirdAttribute;
 
-/**设置x轴标签*/
+/**
+ 设置x轴的标签数据源
+
+ @param xLabels X轴标签数据源数组
+ */
 - (void)updateXLabels:(NSArray *)xLabels;
 
-/**设置y轴数值*/
+/**
+ 设置y轴数值数据源
+
+ @param yValues y轴数值数据源
+ */
 - (void)updateYValue:(NSArray *)yValues;
 
 @end
